0fe27f0a85380066c66cbea561a8425613e3460c
Кирилл <97906485+Ragimov-Kirill@users.noreply.github.com>
2022-03-11 00:06:36 +0300

    Add requisites

---
+++ src/Pages/Requisites.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Requisites extends Component {
+  render() {
+     return (
+       <div>
+          <h3 align ="center">Реквизиты РУП "Гродноэнерго"</h3>
+       </div>
+     )
+   }
+ }
